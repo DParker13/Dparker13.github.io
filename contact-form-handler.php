e78db6cac396e0b4315773e14351ef71aa76339c
Xonor13 <xonor1@gmail.com>
2019-04-01 15:58:03 -0400

    trying to fix contact form

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,27 +1,17 @@
 <?php
 
+	$to = "danielparker134@gmail.com";
 	$name = $_POST['name'];
-	$user_email = $_POST['email'];
+	$from = $_POST['email'];
 	$message = $_POST['message'];
 	
-	$email_from = 'danielparker134@gmail.com';
-	
 	$email_subject ="NEW MESSAGE FROM WEBSITE";
+	$email_body = "Name: " . $name. "\n" . "Email: " . $from . "\n" . "Message: " . $message. "\n";
 	
-	$email_body = "Name: $name.\n".
-					"Email: $user_email.\n".
-						"Message: $message.\n";
-						
-	$to = 'danielparker134@gmail.com';
+	$headers = "From: " . $email_from . "\r\n";
 	
-	$headers = "From: $email_from \r\n";
-	
-	$headers .= "Reply-To: $user_email \r\n";
+	$headers2 = "Reply-To: " . $user_email . "\r\n";
 	
 	mail($to, $email_subject, $email_body, $headers);
-	
-	header("Location: index.html");
-
-
 
 ?>
